fddd7d376e0ff36b3b0d4b284198c63512fb696f
Monster Zhang <1311435497@qq.com>
2020-01-29 17:49:05 +0800

    配置文件

---
+++ config/config.h
@@ -0,0 +1,18 @@
+/*****************************************************/
+/** Author: Zhang DY                                             */
+/**Date: 2020/1/21                                                */
+/**Desription: 配置文件                                          */
+/****************************************************/
+
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<iostream>
+#include<time.h>
+#include<conio.h>
+
+//#define DEBUG
+
+//数独棋盘大小
+constexpr int puzzle_size = 8;
